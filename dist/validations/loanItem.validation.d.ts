3bdd219b2cb3c65d55cedc601cb04a24ecf43be0
tsaqif,xena,ucup <ramadhaniy660@gmail.com>
2026-01-04 17:39:04 +0700

    project

--- dist/validations/loanItem.validation.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const loanItemIdValidation: import("express-validator").ValidationChain[];
-export declare const loanIdParamValidation: import("express-validator").ValidationChain[];
-//# sourceMappingURL=loanItem.validation.d.ts.map
