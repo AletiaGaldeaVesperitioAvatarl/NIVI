87201dee636e336ed1f31a2257791394e735738a
tsaqif,xena,ucup <ramadhaniy660@gmail.com>
2026-01-05 14:42:35 +0700

    TRY

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,62 +1,72 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { UserService } from "../service/user.service";
 import { successResponse } from "../utils/response";
 
 export class UserController {
   constructor(private userService: UserService) {}
 
-  getAll = async (_req: Request, res: Response) => {
-    const users = await this.userService.getAll();
-    return  successResponse(res, "Semua user berhasil diambil", users);
-  };
-
-  getById = async (req: Request, res: Response) => {
-    if (!req.params.id) {
-      throw new Error("Parameter id tidak ditemukan!");
+  getAll = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await this.userService.getAll();
+      return successResponse(res, "Semua user berhasil diambil", users);
+    } catch (err) {
+      next(err);
     }
-    const id = Number(req.params.id);
-    const user = await this.userService.getById(id);
+  };
 
-    if (!user) {
-      throw new Error("User tidak ditemukan");
+  getById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = Number(req.params.id);
+      if (!id) throw new Error("Parameter id tidak valid!");
+
+      const user = await this.userService.getById(id);
+      if (!user) throw new Error("User tidak ditemukan");
+
+      return successResponse(res, "User berhasil ditemukan", user);
+    } catch (err) {
+      next(err);
     }
-
-    return successResponse(res, "User berhasil ditemukan", user);
   };
 
-  create = async (req: Request, res: Response) => {
-    const { name, email, password, role, kelasId } = req.body;
-    const user = await this.userService.createUser({
-      name,
-      email,
-      password,
-      role,
-      kelasId,
-    });
-    successResponse(res, "User berhasil dibuat", user, null, 201);
-  };
-
-  update = async (req: Request, res: Response) => {
-    if (!req.params.id) {
-      throw new Error("Parameter id tidak ditemukan!");
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await this.userService.createUser(req.body);
+      return successResponse(res, "User berhasil dibuat", user, null, 201);
+    } catch (err) {
+      next(err);
     }
-    const id = Number(req.params.id);
-    const data = req.body;
-    const updatedUser = await this.userService.updateUser(id, data);
-    successResponse(res, "User berhasil diperbarui", updatedUser);
   };
 
-  deactivate = async (req: Request, res: Response) => {
-    if (!req.params.id) {
-      throw new Error("Parameter id tidak ditemukan!");
+  update = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = Number(req.params.id);
+      if (!id) throw new Error("Parameter id tidak valid!");
+
+      const updatedUser = await this.userService.updateUser(id, req.body);
+      return successResponse(res, "User berhasil diperbarui", updatedUser);
+    } catch (err) {
+      next(err);
     }
-    const id = Number(req.params.id);
-    const user = await this.userService.deactivateUser(id);
-    successResponse(res, "User berhasil dinonaktifkan", user);
   };
 
-  getStats = async (_req: Request, res: Response) => {
-    const stats = await this.userService.getStats();
-    successResponse(res, "Statistik user berhasil diambil", stats);
+  deactivate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = Number(req.params.id);
+      if (!id) throw new Error("Parameter id tidak valid!");
+
+      const user = await this.userService.deactivateUser(id);
+      return successResponse(res, "User berhasil dinonaktifkan", user);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  getStats = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const stats = await this.userService.getStats();
+      return successResponse(res, "Statistik user berhasil diambil", stats);
+    } catch (err) {
+      next(err);
+    }
   };
 }
